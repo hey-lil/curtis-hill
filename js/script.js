3423559b6ed0c43210faf85b9bc0a82b0493ab37
Lillian Taft and Mai Cook <mai@>
2020-01-14 14:21:37 -0800

    Add background changing buttons

--- js/script.js
+++ js/script.js
@@ -3,11 +3,29 @@ $(document).ready(function() {
     $("#initially-showing").toggle();
     $("#initially-hidden").toggle();
   });
-});
 
-$(document).ready(function() {
   $("#clickable-list").click(function() {
     $("#showing-list").toggle();
     $("#hiddden-list").toggle();
+  });
+
+  $(".clickable-img").click(function() {
+    $("#slide-info-showing").slideToggle();
+    // $("#slide-info-hidden").slideToggle();
+  });
+
+  $("button#less-green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("less-green-background");
+  })
+
+  $("button#yellow-green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-green-background");
+  });
+
+  $("button#dark-green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-green-background");
   });
 });
